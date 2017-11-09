c94bc2241e9ff10bb946c65c4512a7cd1eee650a
manish24online <manish.ranjan@overcart.com>
2017-11-09 22:36:44 +0530

    Added SOAP api files

---
+++ app/code/local/MR/Webapi/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class MR_Webapi_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
+	 
---
+++ app/code/local/MR/Webapi/Model/Newsletter/Subscriber/Api/V2.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * MR Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is published at http://opensource.org/licenses/osl-3.0.php.
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magecore.com so we can send you a copy immediately
+ *
+ * @category MR
+ * @package Api
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+class MR_Webapi_Model_Newsletter_Subscriber_Api_V2
+    extends Overcart_Webapi_Model_Newsletter_Subscriber_Api
+{
+
+}
